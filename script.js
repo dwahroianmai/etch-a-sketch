42dda01d2f333cf8fa56114fb0102d8ef48b71d8
dwahroianmai <105296071+dwahroianmai@users.noreply.github.com>
2023-01-02 01:37:17 +0100

    Add CSS, make canvas resizable, add paint function

---
+++ script.js
@@ -0,0 +1,36 @@
+const canvas = document.querySelector('#canvas');
+
+let side = 48;
+setCanvas();
+
+const size = document.getElementById('size');
+size.addEventListener('mouseup', (e) => {
+  side = e.target.value;
+  setCanvas();
+})
+
+function setCanvas() {
+  removeChildren(canvas);
+  for (let i = 0; i < (side ** 2); i++) {
+    square = document.createElement('div');
+    square.setAttribute('style', `height: ${640/side}px; \
+    width: ${640/side}px; \
+    background-color: white;`)
+    canvas.appendChild(square);
+    paint();
+  }
+}
+
+function paint() {
+  square.addEventListener('mouseover', (e) => {
+    e.target.setAttribute('style', `height: ${640/side}px; \
+    width: ${640/side}px; \
+    background-color: black`);
+  });
+}
+
+function removeChildren(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
